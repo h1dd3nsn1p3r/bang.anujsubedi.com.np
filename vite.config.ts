d1d971148667ebce02f6d31780f783c211f02837
Anuj Subedi <struggleage@gmail.com>
2025-03-07 13:55:40 +0545

    :package: Added: tailwind.
    - :package: Added: tailwind.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-    }),
-  ],
+	plugins: [
+		tailwindcss(),
+		VitePWA({
+			registerType: "autoUpdate",
+		}),
+	],
 });
