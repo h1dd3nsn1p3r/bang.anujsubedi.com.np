2395989c9654546b4d3690f63ff939b12ee5c7cb
Anuj Subedi <struggleage@gmail.com>
2025-03-07 16:00:45 +0545

    :package: Updated: readme.
    - :package: Updated: readme.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bang Search!
+# Bang search!
 
 DuckDuckGo's bang redirects are slow. Add the following URL as a custom search engine to your browser. Enables all of DuckDuckGo's bangs to work, but much faster.
 
@@ -11,3 +11,68 @@ https://bang.anujsubedi.com.np/?q=%s
 DuckDuckGo does their redirects server side. Their DNS is...not always great. Result is that it often takes ages.
 
 I solved this by doing all of the work client side. Once you've went to https://bang.anujsubedi.com.np/ once, the JS is all cache'd and will never need to be downloaded again. Your device does the redirects, not me.
+
+## How to use it?
+
+Add the following URL as a custom search engine to your browser.
+
+```
+https://bang.anujsubedi.com.np/?q=%s
+```
+
+Here's how to do it in some popular browsers:
+
+### Chrome
+
+1. Go to Google Chrome and enter this in URL bar `chrome://settings/search`
+2. Click on `Manage search engines and site search`
+3. Scroll down to `Site search`
+4. Click on add.
+
+```
+Name: Bang Search
+Keyword: !
+URL: https://bang.anujsubedi.com.np/?q=%s
+```
+5. Click on `Add`
+6. Click on three dots on the right side of the search engine you just added and click on `make default`.
+7. Done.
+
+Close the settings tab and try searching for `!yt adele set fire to the rain`. It should redirect you to the YouTube search results for "adele set fire to the rain". All [DuckDuckGo bangs](https://duckduckgo
+.com/bangs) should work.
+
+### Firefox
+
+1. Go to Firefox and click settings.
+2. Click on `Search`
+3. Scroll down to `Search Shortcuts`
+4. Click on `Add search engine`
+
+```
+Name: Bang Search
+Keyword: !
+URL: https://bang.anujsubedi.com.np/?q=%s
+````
+
+5. Click on `Add`
+6. Scroll up and select `Bang Search` as the default search engine.
+7. Done.
+
+Close the settings tab and try searching for `!yt adele set fire to the rain`. 
+
+### Change default search engine
+
+The default search engine is Google. If you want to change it to DuckDuckGo, Yahoo or Bing, follow the steps below:
+
+1. Navigate to [https://bang.anujsubedi.com.np/](bang search) homepage.
+2. Hit `CRTL + SHIFT + I` to open developer tools.
+3. Go to `Application` tab.
+4. Go to `Storage` > `Local Storage`.
+5. Create a new key `default-engine` and set following values.
+
+```
+Google: `g`
+DuckDuckGo: `ddg`
+Yahoo: `y`
+Bing: `b`
+```
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { bangs } from "./bang";
 
 function Home() {
 	const app = document.querySelector<HTMLDivElement>("#app")!;
+
 	app.innerHTML = `
     <div class="container">
       <div class="content-container">
@@ -10,7 +11,7 @@ function Home() {
 					<img src="/logo.svg" alt="Branding" class="logo" />
 				</a>
         <p class="hero">
-					DuckDuckGo's bang redirects are slow. Replace <a href="https://duckduckgo.com/bang.html" target="_blank">DuckDuckGo's</a> bangs with my bang handler.
+					DuckDuckGo's bang redirects are slow. Replace <a href="https://duckduckgo.com/bang.html" target="_blank">DuckDuckGo's</a> bangs with my <a href="https://github.com/h1dd3nsn1p3r/bang.anujsubedi.com.np?tab=readme-ov-file#how-is-it-that-much-faster" target="_blank">bang search handler</a>.
 				</p>
         <div class="url-container"> 
           <input 
@@ -33,7 +34,9 @@ function Home() {
   `;
 
 	const copyButton = app.querySelector<HTMLButtonElement>(".copy-button")!;
+
 	const copyIcon = copyButton.querySelector("img")!;
+
 	const urlInput = app.querySelector<HTMLInputElement>(".url-input")!;
 
 	copyButton.addEventListener("click", async () => {
@@ -47,12 +50,15 @@ function Home() {
 	});
 }
 
-const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
-const defaultBang = bangs.find((b) => b.t === LS_DEFAULT_BANG);
+const defaultBangSearchEngine = localStorage.getItem("default-engine") ?? "g";
+
+const defaultBang = bangs.find((b) => b.t === defaultBangSearchEngine);
 
 function getBangRedirectURL() {
 	const url = new URL(window.location.href);
+
 	const query = url.searchParams.get("q")?.trim() ?? "";
+
 	if (!query) {
 		Home();
 		return null;
@@ -61,8 +67,8 @@ function getBangRedirectURL() {
 	const match = query.match(/!(\S+)/i);
 
 	const bangCandidate = match?.[1]?.toLowerCase();
-	const selectedBang =
-		bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
+
+	const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
 
 	// Remove the first bang from the query
 	const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
@@ -74,6 +80,7 @@ function getBangRedirectURL() {
 		// Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
 		encodeURIComponent(cleanQuery).replace(/%2F/g, "/")
 	);
+
 	if (!searchUrl) return null;
 
 	return searchUrl;
@@ -81,7 +88,9 @@ function getBangRedirectURL() {
 
 function handleRedirect() {
 	const searchUrl = getBangRedirectURL();
+
 	if (!searchUrl) return;
+
 	window.location.replace(searchUrl);
 }
 
